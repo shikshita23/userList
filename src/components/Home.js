1473abb31e6ca872e4beea1c4b30493a66ad2479
Sanjay Guwaju <sanjayguwaju@gmail.com>
2024-06-10 10:12:06 +0000

    refactor: Remove unused code and update data source
    This commit removes the unused `db.json` file and updates the data source in the `Home` component to use the new `data.js` file. The `handleData` function now sets the state with the data from `newData.user` instead of making a fetch request to the server. The `handleDelete` function has been updated to remove the user from the state based on the provided `userId`. This change improves the performance and simplifies the code by eliminating unnecessary network requests.

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,16 +3,18 @@ import { useEffect, useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPenSquare ,faTrash} from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
+import newData from "../data.js";
 
 
 export default function Home() {
 const [data,setData]=useState([]);
+const [userId, setUserId] = useState(null)
 const [users,setUsers]=useState([]);
 async function handleData(){
 try{
-        const res=await fetch("http://localhost:8000/user");
-        const data=await res.json();
-        setData(data);
+        // const res=await fetch("http://localhost:8000/user");
+        // const data=await res.json();
+        setData(newData.user);
     }
     catch(error){
         console.log("error=>",error)
@@ -22,15 +24,13 @@ useEffect(()=>{
     handleData()
 },[])
 
-const handleDelete = () => {
-    
-    fetch('http://localhost:8000/user', {
-      method: 'DELETE'
-    })
-    .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(error => console.error(error));
-  }
+const handleDelete = (userId) => {
+  // Remove the user from the state
+  setData(data.filter(user => user.id !== userId));
+  console.log(`New data: ${JSON.stringify(newData)}`);
+};
+
+console.log("data ---->", data);
 
   return (
     <div className="grid grid-cols-6 border-2 border-black tableSize mt-12 mx-auto bg-white ">
@@ -39,18 +39,20 @@ const handleDelete = () => {
                 <div className="borderElement bg-sky-100">Username</div>
                 <div className="borderElement start bg-sky-100 ">Address</div>
                 <div className="borderElement col-span-2 start bg-sky-100 ">Action</div>      
-        {data.map((user,id)=>{
-            return(
-                <React.Fragment key={id}> 
-                <div className="borderElement">{id+1}</div>
-                <div className="borderElement">{user.name}</div>
-                <div className="borderElement">{user.username}</div>
-                <div className="borderElement"> {user.address}</div> 
-                <div className="borderElement "><FontAwesomeIcon icon={faPenSquare} className="cursor-pointer"/></div>
-                <div className="borderElement "><FontAwesomeIcon icon={faTrash} className="cursor-pointer" onClick={() => handleDelete(user.id)} /></div> 
-                </React.Fragment>
-            )
-        })}
+                {data.map((user) => (
+                  <React.Fragment key={user.id}>
+                    <div className="borderElement">{user.id}</div>
+                    <div className="borderElement">{user.name}</div>
+                    <div className="borderElement">{user.username}</div>
+                    <div className="borderElement">{user.address}</div>
+                    <div className="borderElement">
+                      <FontAwesomeIcon icon={faPenSquare} className="cursor-pointer" />
+                    </div>
+                    <div className="borderElement">
+                      <FontAwesomeIcon icon={faTrash} className="cursor-pointer" onClick={() => handleDelete(user.id)} />
+                    </div>
+                  </React.Fragment>
+                ))}
     </div>
   );
 }
